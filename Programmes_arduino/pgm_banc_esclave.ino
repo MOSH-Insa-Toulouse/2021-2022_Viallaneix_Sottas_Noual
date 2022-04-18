bb0dbfbccbd1215a8e1fb01fa4fa8438adefc1b9
RaphaelNoual <raphael.noual2@gmail.com>
2022-04-18 11:03:39 +0200

    ajout pgm version PCB & banc arduinos indep.

---
+++ Programmes_arduino/pgm_banc_esclave.ino
@@ -0,0 +1,99 @@
+#include<Servo.h>
+Servo myservo;// create servo object to control a servo
+// 
+
+///////////////////// VARIABLES BANC DE TEST
+int nbCycles = 5; // nombre de cycles
+int iCase = 0; // variable pour le switch case de la fonction cycle()
+int x = 0; // incrément de degrés pour banc
+int cycleAccomplis = 0;
+int deltaTheta = 5;
+int SortieAngle = 4;
+int y =0;
+
+
+//////////////////// VARIABLES Flex sensor
+const int flexPin = A1;
+float Vflex = 0;
+const float VCC = 5;
+const float R_DIV = 46500.0;
+const float flatResistance = 26500.0;//resistance à plat
+const float bendResistance = 53000.0; //resistance à 90°
+
+/////////// TRIGGER
+#define trigger 13
+bool drapeau;
+
+
+
+void setup() {
+  myservo.attach(6); // connexion servo sur pin 6
+  pinMode(trigger, INPUT);
+   Serial.begin(9600); //serial port ini
+   //setup flex
+   pinMode(flexPin, INPUT);
+   pinMode(SortieAngle, OUTPUT);  // sets the pin as output
+
+}
+
+void loop() {
+ delay(50);
+
+drapeau = digitalRead(trigger);
+if (drapeau == HIGH){
+  cycle();
+  y = map(x-90, -85,85,0,255);
+  analogWrite(SortieAngle,y);//((2 * (180-deltaTheta))/255)*(x)); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
+}
+//MesureFlexSensor();
+
+//cycle();
+ 
+}
+
+
+void cycle() {
+
+  
+  switch (iCase) {
+    case 0:
+      x = x + 1;
+      myservo.write(x);
+      Serial.print(cycleAccomplis);
+      Serial.print("\t theta : ");
+      Serial.print(x-90);
+      Serial.println(" degrés");
+        if (x >= ((90-deltaTheta)*2)) {iCase = 1;} // 
+     //return x;
+      break;
+    case 1:
+      x = x - 1;
+      myservo.write(x-deltaTheta);
+      Serial.print(cycleAccomplis);
+      Serial.print("\t theta : ");
+      Serial.print(x-deltaTheta);
+      Serial.println(" degrés");
+        if (x <= 0) {
+          iCase = 0;
+          cycleAccomplis += 1;}
+        //if (nbcycle = nbmax){
+          //iCase = 2;
+        //}
+       //break;
+    //case 2:
+      // break;
+    default:
+      break;
+  }
+}
+
+void MesureFlexSensor() {
+
+  //Flex
+ int ADCflex = analogRead(flexPin);
+ float Vflex = ADCflex * VCC /1023.0;
+ float Rflex = R_DIV * (VCC / Vflex -1.0);
+ Serial.println("resistance : " + String(Rflex) + "ohms");
+ float angle = map(Rflex, flatResistance, bendResistance, 0, 90);
+ Serial.println("Bend : " + String(angle) + " degrés");
+ }
